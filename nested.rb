c1976be0f18975b806725ef6bff1925382635c12
Usman Pervaiz <de.usman@yahoo.com>
2017-02-20 00:15:31 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,14 +56,11 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages].join()
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP, C"]
-# }
 
 	programmer_hash = 
  		{
@@ -79,7 +77,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+     {:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP, C"]}}.merge(programmer_hash)
     
 end
 
@@ -101,8 +99,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info   
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +119,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+programmer_hash
 end
